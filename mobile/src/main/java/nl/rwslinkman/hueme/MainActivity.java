b5a5d6a1d41fe2ed832f7b68a9fb66c96c7f8437
Rick Slinkman <rw.slinkman@gmail.com>
2015-08-02 18:12:40 +0200

    Reconnected MainActivity to HueService after losing bond while implementing new design structure

--- mobile/src/main/java/nl/rwslinkman/hueme/MainActivity.java
+++ mobile/src/main/java/nl/rwslinkman/hueme/MainActivity.java
@@ -1,33 +1,61 @@
 package nl.rwslinkman.hueme;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.rwslinkman.hueme.fragments.GroupsFragment;
+import nl.rwslinkman.hueme.fragments.InfoFragment;
+import nl.rwslinkman.hueme.fragments.LightsFragment;
 import nl.rwslinkman.hueme.fragments.NavigationDrawerFragment;
+import nl.rwslinkman.hueme.hueservice.HueBroadcaster;
+import nl.rwslinkman.hueme.hueservice.HueService;
+import nl.rwslinkman.hueme.hueservice.HueServiceStateListener;
 import nl.rwslinkman.hueme.navigation.NavigationDrawerCallbacks;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerCallbacks {
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
+public class MainActivity extends ActionBarActivity implements NavigationDrawerCallbacks, HueServiceStateListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private final BroadcastReceiver hueUpdateReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            Log.d(TAG, "Broadcast received: " + action);
+        }
+    };
     private NavigationDrawerFragment mNavigationDrawerFragment;
     private Toolbar mToolbar;
+    private List<Fragment> fragmentList;
+    private HueMe app;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mToolbar);
+
+        fragmentList = new ArrayList<>();
+        fragmentList.add(LightsFragment.newInstance());
+        fragmentList.add(GroupsFragment.newInstance());
+        fragmentList.add(InfoFragment.newInstance());
 
         mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.fragment_drawer);
 
@@ -38,48 +66,63 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
+    protected void onResume()
+    {
+        super.onResume();
+
+        app = (HueMe) getApplication();
+        if(!app.isServiceReady())
+        {
+            app.subscribeHueServiceState(this);
+        }
+        else
+        {
+            // Subscribe to HueService for display events
+            HueService hueService = app.getHueService();
+            hueService.registerReceiver(hueUpdateReceiver, getDisplayUpdatesFilter());
+            Log.d(TAG, "HueService obtained in MainActivity and registered to updates");
+        }
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position)
+    {
         // update the main content by replacing fragments
         Toast.makeText(this, "Menu item selected -> " + position, Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         if (mNavigationDrawerFragment.isDrawerOpen())
+        {
             mNavigationDrawerFragment.closeDrawer();
+        }
         else
+        {
             super.onBackPressed();
+        }
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+    public void onHueServiceReady()
+    {
+        HueService service = app.getHueService();
+        service.registerReceiver(hueUpdateReceiver, this.getDisplayUpdatesFilter());
     }
-
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onHueServiceHalted()
+    {
+        Log.d(TAG, "Hue service no longer available");
     }
 
-
+    private IntentFilter getDisplayUpdatesFilter()
+    {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(HueBroadcaster.DISPLAY_NO_BRIDGE_STATE);
+        return intentFilter;
+    }
 }
